7b45773de9e350ec48c753b0175cf4286a12c75f
@vzapupok <vzapupok@gmail.com>
2024-02-22 12:35:25 +0300

    add some styles on side bar

--- admin-space/src/components/SideBar/SideBar.tsx
+++ admin-space/src/components/SideBar/SideBar.tsx
@@ -16,20 +16,20 @@ const SideBar = () => {
 
     }
     return (
-        <div className='h-dvh flex flex-col items-center dark:bg-dark_1'>
+        <div className='h-dvh flex flex-col items-center border-r-2 dark:bg-dark_1'>
             <div className='mt-12 flex flex-col items-center'>
                 <div className='mb-24'>
                     <img width={60} height={60} src={logo} alt="#"/>
                 </div>
                 <div className='flex flex-col items-center gap-6'>
-                    <Home  width={60} height={60}  fill={'#D1D1D1'}/>
-                    <Email width={60} height={60} fill={'#D1D1D1'} />
-                    <ContactBook width={60} height={60} fill={'#D1D1D1'}/>
-                    <Coin width={60} height={60} fill={'#D1D1D1'}/>
-                    <Dashboard width={60} height={60} fill={'#D1D1D1'}/>
-                    <Invoices width={60} height={60} fill={'#D1D1D1'} />
-                    <Money width={60} height={60} fill={'#D1D1D1'}/>
-                    <Ticket width={60} height={60} fill={'#D1D1D1'}/>
+                    <Home  width={32} height={32}  fill={'#D1D1D1'}/>
+                    <Email width={32} height={32} fill={'#D1D1D1'} />
+                    <ContactBook width={32} height={32} fill={'#D1D1D1'}/>
+                    <Coin width={32} height={32} fill={'#D1D1D1'}/>
+                    <Dashboard width={32} height={32} fill={'#D1D1D1'}/>
+                    <Invoices width={32} height={32} fill={'#D1D1D1'} />
+                    <Money width={32} height={32} fill={'#D1D1D1'}/>
+                    <Ticket width={32} height={32} fill={'#D1D1D1'}/>
                 </div>
             </div>
         </div>
